31cc3f47d1311977f86890cfc8b9d11fc6bac18a
Satya Pavan <pavan.simham@gmail.com>
2018-10-13 11:21:58 +0530

    a little more changes

---
+++ js/404.js
@@ -0,0 +1,10 @@
+        (function(){
+            console.log("Testing...");
+            
+        function ver_01() {
+            console.log("ver_01");
+        }
+
+        ver_01();
+
+        })();
